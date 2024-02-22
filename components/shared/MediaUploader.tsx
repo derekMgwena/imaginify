48d06f97bd08a4676b333941bc3cdfea79c1b073
derekMgwena <derekmakhubela@gmail.com>
2024-02-22 17:43:59 +0200

    almost done with setting up folder structures

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="dm_imaginify"
       options={{
         multiple: false,
         resourceType: "image",
